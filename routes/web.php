147425e07c1f3650bba71b7fb4099dd9b93fda07
ahmd-ramzan <iamahmdrmzn@gmail.com>
2023-12-07 22:04:40 +0500

    basic test case

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(ProjectsController::class)->group(function () {
+    Route::get('/projects', 'index');
+    Route::post('/projects', 'store');
 });
